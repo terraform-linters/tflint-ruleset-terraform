45f7f0664179b5d81ab722154056bac71aea981e
Robert DiMartino <robert@dimartino.dev>
2024-05-20 13:39:30 -0400

    Add ranges to terraform_required_version errors

--- rules/terraform_required_version_test.go
+++ rules/terraform_required_version_test.go
@@ -1,8 +1,10 @@
 package rules
 
 import (
+	"path/filepath"
 	"testing"
 
+	"github.com/hashicorp/hcl/v2"
 	"github.com/terraform-linters/tflint-plugin-sdk/helper"
 )
 
@@ -25,6 +27,17 @@ terraform {}
 				{
 					Rule:    NewTerraformRequiredVersionRule(),
 					Message: "terraform \"required_version\" attribute is required",
+					Range: hcl.Range{
+						Filename: "module.tf",
+						Start: hcl.Pos{
+							Line:   2,
+							Column: 1,
+						},
+						End: hcl.Pos{
+							Line:   2,
+							Column: 10,
+						},
+					},
 				},
 			},
 		},
@@ -53,6 +66,31 @@ terraform {
 `,
 			Expected: helper.Issues{},
 		},
+		{
+			Name: "no terraform block",
+			Content: `
+locals {
+	foo = "bar"
+}
+`,
+			Expected: helper.Issues{
+				{
+					Rule:    NewTerraformRequiredVersionRule(),
+					Message: "terraform \"required_version\" attribute is required",
+					Range: hcl.Range{
+						Filename: "module.tf",
+						Start: hcl.Pos{
+							Line:   1,
+							Column: 1,
+						},
+						End: hcl.Pos{
+							Line:   1,
+							Column: 1,
+						},
+					},
+				},
+			},
+		},
 	}
 
 	rule := NewTerraformRequiredVersionRule()
@@ -65,6 +103,87 @@ terraform {
 			}
 			runner := helper.TestRunner(t, files)
 
+			if err := rule.Check(runner); err != nil {
+				t.Fatal(err)
+			}
+
+			helper.AssertIssues(t, tc.Expected, runner.Issues)
+		})
+	}
+}
+
+func Test_TerraformRequiredVersionRuleMultipleFiles(t *testing.T) {
+	cases := []struct {
+		Name     string
+		Files    []string
+		Expected helper.Issues
+	}{
+		{
+			Name:  "has terraform.tf and main.tf",
+			Files: []string{"modules/foo/main.tf", "modules/foo/terraform.tf"},
+			Expected: helper.Issues{
+				{
+					Rule:    NewTerraformRequiredVersionRule(),
+					Message: "terraform \"required_version\" attribute is required",
+					Range: hcl.Range{
+						Filename: filepath.FromSlash("modules/foo/terraform.tf"),
+						Start: hcl.Pos{
+							Line:   1,
+							Column: 1,
+						},
+						End: hcl.Pos{
+							Line:   1,
+							Column: 1,
+						},
+					},
+				},
+			},
+		},
+		{
+			Name:  "has main.tf",
+			Files: []string{"modules/foo/outputs.tf", "modules/foo/main.tf", "modules/foo/variables.tf"},
+			Expected: helper.Issues{
+				{
+					Rule:    NewTerraformRequiredVersionRule(),
+					Message: "terraform \"required_version\" attribute is required",
+					Range: hcl.Range{
+						Filename: filepath.FromSlash("modules/foo/main.tf"),
+						Start: hcl.Pos{
+							Line:   1,
+							Column: 1,
+						},
+						End: hcl.Pos{
+							Line:   1,
+							Column: 1,
+						},
+					},
+				},
+			},
+		},
+		{
+			Name:  "has neither terraform.tf or main.tf",
+			Files: []string{"modules/foo/variables.tf", "modules/foo/outputs.tf"},
+			Expected: helper.Issues{
+				{
+					Rule:    NewTerraformRequiredVersionRule(),
+					Message: "terraform \"required_version\" attribute is required",
+					Range: hcl.Range{
+						Filename: filepath.FromSlash("modules/foo/terraform.tf"),
+					},
+				},
+			},
+		},
+	}
+
+	rule := NewTerraformRequiredVersionRule()
+
+	for _, tc := range cases {
+		t.Run(tc.Name, func(t *testing.T) {
+			var files = map[string]string{}
+			for _, filename := range tc.Files {
+				files[filepath.FromSlash(filename)] = ""
+			}
+			runner := helper.TestRunner(t, files)
 			if err := rule.Check(runner); err != nil {
 				t.Fatal(err)
 			}
